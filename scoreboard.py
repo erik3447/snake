4ba2e9ae798977b072e47eec6ff17cbc58292d6e
erik3447 <erik.reimers@live.de>
2022-12-17 23:05:23 +0100

    change code

--- scoreboard.py
+++ scoreboard.py
@@ -11,10 +11,18 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.goto(0, 260)
         self.update_scoreboard()
+        self.high_score = 0
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
+    def new_highscore(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+
+
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
+
     def update_scoreboard(self):
         self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
     def score_refresh(self):
